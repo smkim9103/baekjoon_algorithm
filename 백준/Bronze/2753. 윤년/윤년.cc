2a2106d1a68905d458ea92038ab686533a25bc37
smkim9103 <smkim9103@gmail.com>
2024-08-02 12:02:57 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+  int a;
+  cin>>a;
+  if(a%4==0 && (a%100!=0 || a%400==0))
+  {
+    cout<<"1";
+  }
+  else
+  {
+    cout<<"0";
+  }
+}
