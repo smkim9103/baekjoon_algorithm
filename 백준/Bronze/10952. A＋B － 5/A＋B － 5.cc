dafb8e68e0e6d3deb559abb8739570e2c8d41759
smkim9103 <smkim9103@gmail.com>
2024-08-05 17:38:55 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+  int a,b;
+  
+  while(true)
+    {
+      cin>>a>>b;
+
+      if(a==0 && b==0)
+      {
+        break;
+      }
+      else
+      {
+        cout<<a+b<<endl;
+      }
+    }
+}
