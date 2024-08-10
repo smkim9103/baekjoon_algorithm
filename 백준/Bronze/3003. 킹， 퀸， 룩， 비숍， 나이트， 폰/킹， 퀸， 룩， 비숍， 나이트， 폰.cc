ed16a99f56a1268f4a02b27f368df6909a6d8e0a
smkim9103 <smkim9103@gmail.com>
2024-08-10 13:56:15 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,22 @@
+#include<cstdio>
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    /*
+    string a;
+    cin>>a;
+    int cnt=0;
+    for(int i=0;i<a.size();i++)
+        {
+            if(a[i]==" ")
+                cnt++;
+        }
+    cout<<cnt+1;
+    */
+    int a,b,c,d,e,f;
+    cin>>a>>b>>c>>d>>e>>f;
+    cout<<1-a<<" "<<1-b<<" "<<2-c<<" "<<2-d<<" "<<2-e<<" "<<8-f;
+}
