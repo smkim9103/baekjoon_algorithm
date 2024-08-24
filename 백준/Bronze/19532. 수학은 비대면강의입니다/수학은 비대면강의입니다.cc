cb1665f88b93e78090b213f5369887725db9c08a
smkim9103 <smkim9103@gmail.com>
2024-08-24 13:23:42 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    int a,b,c,e,d,f;
+    cin>>a>>b>>c>>d>>e>>f;
+
+    for(int x=-999;x<1000;x++)
+        {
+            for(int y=-999;y<1000;y++)
+                {
+                    if(a*x+b*y==c && d*x+e*y==f)
+                    {
+                        cout<<x<<" "<<y;
+                        return 0;
+                    }
+                }
+        }
+}
