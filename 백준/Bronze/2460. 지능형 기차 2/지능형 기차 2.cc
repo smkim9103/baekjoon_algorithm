9127adfee1be88f284e376b4df60bdb9d29efadc
smkim9103 <smkim9103@gmail.com>
2024-11-02 12:43:09 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int max=0, off, on;
+    int total=0;
+
+    for(int i=0;i<10;i++)
+    {
+        cin>>off>>on;
+
+        total=total-off+on;
+
+        if(total>max)
+        {
+            max=total;
+        }
+    }
+
+    cout<<max;
+    return 0;
+}
